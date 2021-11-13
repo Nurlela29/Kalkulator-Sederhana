9a78806c31b0684f436f28f94bd235b63e9c579c
Nurlela29 <87569806+Nurlela29@users.noreply.github.com>
2021-11-13 22:00:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function insert(num){
+	document.form.textarea.value = document.form.textarea.value + num;
+}
+
+function persen(num){
+	document.form.textarea.value = document.form.textarea.value / "100";
+}
+
+
+function equal(){
+	var hasil = document. form.textarea.value;
+	document.form.textarea.value = eval(hasil);
+}
+
+function clean(){
+	document.form.textarea.value = "";
+}
+
+function back(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
